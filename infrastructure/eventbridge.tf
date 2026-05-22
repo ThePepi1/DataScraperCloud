624d761091be5b107e5e72f601b5c0db4f87c3b7
Prle04 <prlincevic04@gmail.com>
2026-05-22 23:42:33 +0200

    Feat add hacker news lambda

--- infrastructure/eventbridge.tf
+++ infrastructure/eventbridge.tf
@@ -6,13 +6,13 @@ resource "aws_cloudwatch_event_rule" "bronze_hn_daily" {
 resource "aws_cloudwatch_event_target" "bronze_hn" {
   rule      = aws_cloudwatch_event_rule.bronze_hn_daily.name
   target_id = "bronze-hn-lambda"
-  arn       = aws_lambda_function.bronze_hn.arn
+  arn       = aws_lambda_function.bronze_layer.arn
 }
 
 resource "aws_lambda_permission" "eventbridge_bronze_hn" {
   statement_id  = "AllowEventBridgeInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.bronze_hn.function_name
+  function_name = aws_lambda_function.bronze_layer.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.bronze_hn_daily.arn
 }
